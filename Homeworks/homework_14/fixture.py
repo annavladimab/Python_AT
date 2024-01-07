fe4287ecc50b1a8c601c9da341c95c78d0c9767d
annaabramova <annaavbramova@gmail.com>
2024-01-08 01:23:38 +0400

    update: hw 14 1.one test function containing actions 2.fixture pre-req was created

--- Homeworks/homework_14/fixture.py
+++ Homeworks/homework_14/fixture.py
@@ -31,3 +31,9 @@ def setup(request, driver_init):
 def login_data():
     return {"user": "a_test", "password": "a_test12345"}
 
+
+@pytest.fixture(scope="class")
+def category_setup(request, setup, login_data):
+    request.cls.login_page.open_tab_page()
+    request.cls.login_page.login(user=login_data["user"], password=login_data["password"])
+    request.cls.login_page.click_on_login_btn()
--- Homeworks/homework_14/test_site.py
+++ Homeworks/homework_14/test_site.py
@@ -1,36 +1,28 @@
 import pytest
 from selenium import webdriver
 from selenium.common import NoSuchElementException
+
 import fixture
-from fixture import driver_init, login_data, driver_init, setup
+from fixture import driver_init, login_data, setup, category_setup
 
 
 @pytest.mark.usefixtures("setup")
 class TestLogin:
 
-    @pytest.fixture(scope="function", autouse=True)
-    def test_login_page_displayed(self):
+    def test_login_scenario(self, login_data):
         self.login_page.open_tab_page()
         assert self.login_page.is_login_page_username_displayed()
         assert self.login_page.is_login_page_password_displayed()
-
-    def test_successful_login(self, request, login_data):
         self.login_page.login(user=login_data["user"], password=login_data["password"])
         self.login_page.click_on_login_btn()
         assert "Log out" in self.login_page.get_logout_button_text().strip()
         assert "Welcome a_test" in self.login_page.get_welcome_text().strip()
 
 
-@pytest.mark.usefixtures("setup")
+@pytest.mark.usefixtures("setup", "category_setup")
 class TestCategory:
 
-    def pre_req(self, login_data):
-        self.login_page.open_tab_page()
-        self.login_page.login(user=login_data["user"], password=login_data["password"])
-        self.login_page.click_on_login_btn()
-
-    def test_product_name_and_price(self, login_data):
-        self.pre_req(login_data)
+    def test_product_name_and_price(self):
         self.category_page.click_monitor_button()
         self.category_page.click_category_product_button()
 
@@ -40,8 +32,7 @@ class TestCategory:
         expected_product_price = "$400 *includes tax"
         assert expected_product_price in self.category_page.get_product_price().strip()
 
-    @pytest.mark.dependency(depends=["TestCategory::test_product_name_and_price"])
-    def test_added_product_name_and_price(self):
+       # def test_added_product_name_and_price(self):
         self.category_page.click_add_button()
         self.category_page.click_cart_button()
 
